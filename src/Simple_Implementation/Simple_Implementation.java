ec177cdd149351646e6167f34c74f3813829ccd6
xhinsade <nekochii57@gmail.com>
2024-09-22 15:58:12 +0800

    added simple implementation

---
+++ src/Simple_Implementation/Simple_Implementation.java
@@ -0,0 +1,18 @@
+
+package Simple_Implementation;
+
+
+public class Simple_Implementation {
+
+    
+    public static void main(String[] args) {
+        
+        String [] fruits = {"Watermelon", "Grapes", "Apple", "Orange"};
+               
+        System.out.println(fruits[1]);
+        System.out.println(fruits[3]);
+        System.out.println(fruits[2]);
+        
+    }
+    
+}
