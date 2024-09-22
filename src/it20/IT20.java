40f32901002e38b37ed8990a31a6ecfe9484751a
xhinsade <nekochii57@gmail.com>
2024-09-22 15:26:49 +0800

    Introduction of Data Structure and Algorithm

---
+++ src/it20/IT20.java
@@ -0,0 +1,13 @@
+
+package it20;
+
+public class IT20 {
+
+   
+    public static void main(String[] args) {
+       
+        
+        System.out.println(" Welcome to Data Structure and Algorithm");
+    }
+    
+}
