4af6eded7a16408e3f2fe5e6eba0e61997b5bfa0
xhinsade <nekochii57@gmail.com>
2024-09-22 16:19:30 +0800

    added description for displaying via for loop

--- src/DisplayingViaForLoop/Display_Via_For_Loop.java
+++ src/DisplayingViaForLoop/Display_Via_For_Loop.java
@@ -1,19 +1,23 @@
 package DisplayingViaForLoop;
 
-
 public class Display_Via_For_Loop {
 
-    
     public static void main(String[] args) {
-        
-        String [] charactersName = {"Lilith", "Mavis","Luffy", "Ace", "Zoro"};
-                
-                for (int i = 0; i < charactersName.length; i++){
-                
-                
-                System.out.println(charactersName[i]);
-        
+
+        //declaring variable to the array 
+        String[] charactersName = {"Lilith", "Mavis", "Luffy", "Ace", "Zoro"};
+
+        //procedure to print the names inside the array
+        for (int i = 0; i < charactersName.length; i++) {
+            System.out.println(charactersName[i]);
+
+            //output
+            /*Lilith
+           Mavis
+           Luffy
+            Ace
+            Zoro */
+        }
     }
-    }
-    
+
 }
