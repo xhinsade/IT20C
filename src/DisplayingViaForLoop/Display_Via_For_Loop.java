6f6d84724ec0e494e20cd1a6b0c5b3bf636b63a0
xhinsade <nekochii57@gmail.com>
2024-09-22 16:15:01 +0800

    added Display via for Loop

---
+++ src/DisplayingViaForLoop/Display_Via_For_Loop.java
@@ -0,0 +1,19 @@
+package DisplayingViaForLoop;
+
+
+public class Display_Via_For_Loop {
+
+    
+    public static void main(String[] args) {
+        
+        String [] charactersName = {"Lilith", "Mavis","Luffy", "Ace", "Zoro"};
+                
+                for (int i = 0; i < charactersName.length; i++){
+                
+                
+                System.out.println(charactersName[i]);
+        
+    }
+    }
+    
+}
