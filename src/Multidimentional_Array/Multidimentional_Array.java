c274a2e36351ad454ea2ba7d66e4213a1899c3a5
xhinsade <nekochii57@gmail.com>
2024-09-22 16:45:42 +0800

    added description for multidimentional arrays

--- src/Multidimentional_Array/Multidimentional_Array.java
+++ src/Multidimentional_Array/Multidimentional_Array.java
@@ -4,23 +4,30 @@ public class Multidimentional_Array {
 
     public static void main(String[] args) {
 
+        //declaring variable inside the array
         String[][] nameAndPassword = {
-            { "Kristine", " gd244deq"},
-            { "Katelyn", " gd2qmkg34"},
-            { "Jovan", " gdfhyyreq"}, 
-            { "Israel", " lgre45eq"},
-            { "Justine", "ldssu635"},
-        };
-        
-            
-            for (int i = 0; i < nameAndPassword.length; i++){
-                for (int j = 0; j < nameAndPassword[i].length; j++){
-                   
-                    
-                    System.out.print(nameAndPassword[i][j]);
-                }
-                System.out.println(" ");
+            {"Kristine", " gd244deq"},
+            {"Katelyn", " gd2qmkg34"},
+            {"Jovan", " gdfhyyreq"},
+            {"Israel", " lgre45eq"},
+            {"Justine", "ldssu635"},};
+
+        // procedures, printing name inside the array
+        for (int i = 0; i < nameAndPassword.length; i++) {
+            for (int j = 0; j < nameAndPassword[i].length; j++) {
+
+                System.out.print(nameAndPassword[i][j] + " ");
             }
+            System.out.println(" ");
+        }
+        //output
+        /*
+        Kristine gd244deq  
+        Katelyn gd2qmkg34  
+        Jovan gdfhyyreq  
+        Israel lgre45eq  
+        Justine ldssu635 
+        */
 
         
     }
