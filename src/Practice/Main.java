f977099c17f7df363cf12212829879fc8ae89b7f
xhinsade <nekochii57@gmail.com>
2024-09-24 16:51:56 +0800

    practice only, display ug add sa fruit

---
+++ src/Practice/Main.java
@@ -0,0 +1,19 @@
+
+package Practice;
+
+
+public class Main {
+    
+    public static void main(String []args){
+    
+        FruitSaladMaker list = new FruitSaladMaker();
+        
+    System.out.println("Let's Make a fruit Salad ");
+    System.out.println("-------------------------");
+    System.out.println(" ");
+    
+    
+    list.AddFruit();
+    list.DisplayFruit();
+    }
+}
