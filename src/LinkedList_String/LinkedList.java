a959a00289b3c1ba3f747eabab3089ea307fe7d2
xhinsade <nekochii57@gmail.com>
2024-10-01 15:07:03 +0800

    Added the Main Class, Node Class and Linkedlist class

---
+++ src/LinkedList_String/LinkedList.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinkedList_String;
+
+/**
+ *
+ * @author ITLAB1-PC18-STUDENT
+ */
+public class LinkedList {
+    
+    private Node head;
+    
+    public void add(String data){
+    
+    Node newNode = new Node(data);
+    if (head == null){
+        head = newNode;
+        return;
+    }
+    Node current = head;
+    while(current.next != null){
+        current = current.next;
+    }
+    current.next = newNode;
+  }
+}
---
+++ src/LinkedList_String/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinkedList_String;
+
+/**
+ *
+ * @author ITLAB1-PC18-STUDENT
+ */
+public class Main {
+    
+    public static void main(String [] args){
+    LinkedList list = new LinkedList();
+    
+    list.add(" Agora Hills - Doja Cat");
+    list.add(" Fantasize - Ariana Grande");
+    list.add(" Sining - Dionela ft. Jay R");
+    list.add(" Sugar - Florida");
+    list.add(" Lil Kasalanan - Hev Abi");
+    list.add(" Walang Alam -  Hev Abi");
+    
+    System.out.println(" Current Music List");
+    System.out.println(" ---------------------------- ");
+    }
+}
