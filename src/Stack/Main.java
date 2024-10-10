ce050ee3633ab170cf680cb93a7fce93bbedc11b
Kristine <167082524+xhinsade@users.noreply.github.com>
2024-10-10 12:50:34 +0000

    Adde the Scanner and  Stack

--- src/Stack/Main.java
+++ src/Stack/Main.java
@@ -2,11 +2,12 @@
 
 package Stack;
 
+import java.util.Scanner;
 
 public class Main {
-    
     public static void main (String [] args){
-        
+        Scanner sc = new Scanner(System.in);
+        Stack stack = new Stack();
         
         
     }
