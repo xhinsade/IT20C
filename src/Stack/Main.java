6bb28b9948e9f700a523b11d9fe4400a6c26d733
Kristine <167082524+xhinsade@users.noreply.github.com>
2024-10-11 00:35:36 +0000

    Added stack Size

--- src/Stack/Main.java
+++ src/Stack/Main.java
@@ -7,8 +7,15 @@ import java.util.Scanner;
 public class Main {
     public static void main (String [] args){
         Scanner sc = new Scanner(System.in);
-        Stack stack = new Stack();
+        Stack stack = new Stack(StackSize);
         
+        System.out.print("Enter the size of stack: ");
+        int StackSize = sc.nextInt();
+
+        boolean running = true;
+
         
+
+
     }
 }
