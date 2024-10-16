8fdf88e6d0edf32d728a8e0a4df69f87c114225e
xhinsade <nekochii57@gmail.com>
2024-10-16 17:17:32 +0800

    Added the queue class

---
+++ src/Queue/Queue.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Queue;
+
+/**
+ *
+ * @author ITLAB1-PC05-STUDENT
+ */
+public class Queue {
+    
+}
