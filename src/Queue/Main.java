cdb28acd447f61af07216eba6537d07ccb37cf0c
Kristine <nekochii57@gmail.com>
2024-10-17 18:37:38 +0800

    Added instances for the object and the Scanner

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -13,7 +13,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        CustomerQueue customerQueue = new CustomerQueue();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        
     }
     
 }
