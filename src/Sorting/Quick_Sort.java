30baf8d2f81cab1646d83039b960668992f4d25e
Kristine <nekochii57@gmail.com>
2024-11-28 23:09:06 +0800

    Added method for display array

--- src/Sorting/Quick_Sort.java
+++ src/Sorting/Quick_Sort.java
@@ -32,3 +32,10 @@ public class Quick_Sort {
 
         return i + 1;
     }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
