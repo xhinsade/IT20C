bdcf4a88a42b42fb0eb00154751c7f0ad2479841
Kristine <nekochii57@gmail.com>
2024-11-28 22:56:42 +0800

    Added the method to print array

--- src/Sorting/Bubble_Sort.java
+++ src/Sorting/Bubble_Sort.java
@@ -14,3 +14,10 @@ public class Bubble_Sort {
             }
         }
     }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
