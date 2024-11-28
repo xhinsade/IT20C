fea5470d45a5461170458681c62c2287dc36b1c0
Kristine <nekochii57@gmail.com>
2024-11-28 23:01:05 +0800

    Added method to print array

--- src/Sorting/Insertion_Sort.java
+++ src/Sorting/Insertion_Sort.java
@@ -14,4 +14,11 @@ public class Insertion_Sort {
             arr[j + 1] = key;
         }
     }
-  
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    
