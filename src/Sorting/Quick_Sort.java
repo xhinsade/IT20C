54b4f4e7110ae3fc5544ecf2fa7e63fca567ad57
Kristine <nekochii57@gmail.com>
2024-11-28 23:07:08 +0800

    Added object instance for array

--- src/Sorting/Quick_Sort.java
+++ src/Sorting/Quick_Sort.java
@@ -10,3 +10,17 @@ public class Quick_Sort {
             quickSort(arr, pi + 1, high);
         }
     }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
