68355745bc99c072d1ae513a1af4af4285f22155
Kristine <nekochii57@gmail.com>
2024-11-28 23:11:45 +0800

    Added scanner

--- src/Sorting/Selection_Sort.java
+++ src/Sorting/Selection_Sort.java
@@ -23,3 +23,30 @@ public static void printArray(int[] arr) {
         }
         System.out.println();
 }
+
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input the size of the array
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        // Create array and take inputs
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        // Perform selection sort
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+
+        scanner.close();
+    }
+}
