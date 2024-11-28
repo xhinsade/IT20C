b7a5033fd25cc881da45dee16130e98cd441dc6e
Kristine <nekochii57@gmail.com>
2024-11-28 23:06:34 +0800

    Added method for quick sort

---
+++ src/Sorting/Quick_Sort.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Quick_Sort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
