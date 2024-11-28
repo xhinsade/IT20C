82840629692b81ed15561213080409547aa67773
Kristine <nekochii57@gmail.com>
2024-11-28 23:03:52 +0800

    Added method to handle merge sorting

---
+++ src/Sorting/Merge_Sort.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Merge_Sort {
+    
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+  
